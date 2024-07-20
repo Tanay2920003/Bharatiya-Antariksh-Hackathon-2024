02bcd6467a9adbab38f9b46bee0ffd340fcdb8af
Tanay <tanayanandmishra@gmail.com>
2024-07-21 01:17:19 +0530

    Working Speech

--- Voice enabled user interface for geospatial map based/main.py
+++ Voice enabled user interface for geospatial map based/main.py
@@ -1,4 +1,8 @@
+import logging
 from google.cloud import speech_v1p1beta1 as speech
+
+# Suppress unnecessary logs
+logging.basicConfig(level=logging.ERROR)
 
 def transcribe_audio(audio_file_path):
     with open(audio_file_path, 'rb') as audio_file:
@@ -6,15 +10,15 @@ def transcribe_audio(audio_file_path):
     audio = speech.RecognitionAudio(content=content)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code='en-US',  # Change to your desired language
+        sample_rate_hertz=16000,  # Ensure this matches the sample rate of your mono audio
+        language_code='en-US',
     )
-    client = speech.SpeechClient.from_service_account_json('C:/Users/Tanay/Desktop/Bharatiya-Antariksh-Hackathon-2024/Voice enabled user interface for geospatial map based/hybrid-sunbeam-429814-a0-7aae21dccd0b.json')
+    client = speech.SpeechClient.from_service_account_json(
+        'C:/Users/Tanay/Desktop/Bharatiya-Antariksh-Hackathon-2024/Voice enabled user interface for geospatial map based/hybrid-sunbeam-429814-a0-7aae21dccd0b.json'
+    )
     response = client.recognize(config=config, audio=audio)
     for result in response.results:
         print(f"Transcript: {result.alternatives[0].transcript}")
 
 # Example usage:
-
-transcribe_audio('C:/Users/Tanay/Downloads/audio.mp3')
-
+transcribe_audio('C:/Users/Tanay/Downloads/audio2.wav')
