5c54f36a1f22846bb95d753ec2b092501d3ef0e6
Tanay <tanayanandmishra@gmail.com>
2024-07-23 14:46:44 +0530

    Togge Button Added  to switch from normal to temperature

--- leaflet/leaflet/app.py
+++ leaflet/leaflet/app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 # Example data (replace with your dynamic data source)
 markers = [
     {"name": "New Delhi, India", "lat": 28.6790, "lng": 77.0697},
-    {"name": "Mumbai (Bombay), India", "lat": 19.0760, "lng": 72.8777},
-    {"name": "Lucknow, India", "lat": 26.8500, "lng": 80.9499}
+
 ]
 
 @app.route('/')
