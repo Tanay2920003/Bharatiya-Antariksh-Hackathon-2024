67a40dac9340abcb18a34ec16daccd88670bdeb0
Tanay <tanayanandmishra@gmail.com>
2024-07-26 04:31:21 +0530

    port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
@@ -17,4 +19,5 @@ def get_markers():
     return jsonify(markers)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
